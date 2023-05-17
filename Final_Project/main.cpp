231bf7d59c7b62e6a994055fdd8fef0e9b27dada
DorisTmaple <toffmaple@gmail.com>
2023-05-17 11:58:47 +0800

    DTang---week14_切換關節

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -5,8 +5,8 @@ GLMmodel * head = NULL;
 GLMmodel * body = NULL;
 GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
-int show[4] = {1,1,1,1};
-int ID = 0; /// 0->頭、1->身、2->上手臂、3->下手臂
+int show[4] = {0,1,1,1};
+int ID = 3; /// 0->頭、1->身、2->上手臂、3->下手臂
 void keyboard(unsigned char key, int x, int y){
     if(key=='0') ID = 0;
     if(key=='1') ID = 1;
@@ -43,27 +43,40 @@ void display(){
         if(ID == 1) glColor3f(1,0,0);
         else glColor3f(1,1,1);
         if(show[1]) glmDraw(body, GLM_MATERIAL);
+
         glPushMatrix();
-            glTranslatef(teapotx, teapoty, 0);
+            glTranslatef(-1.166666, 0.466667, 0);
+            glRotatef(angle,0,0,1);
+            glTranslatef(1.166666, -0.466667, 0);
 
             if(ID == 2) glColor3f(1,0,0);
             else glColor3f(1,1,1);
             if(show[2]) glmDraw(uparmR, GLM_MATERIAL);
+
+            glPushMatrix();
+                glTranslatef(-1.933332, +0.133333, 0);
+                glRotatef(angle,0,0,1);
+                glTranslatef(1.933332, -0.133333, 0);
+
+                if(ID == 3) glColor3f(1,0,0);
+                else glColor3f(1,1,1);
+                if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);
+            glPopMatrix();
         glPopMatrix();
 
-        if(ID == 3) glColor3f(1,0,0);
-        else glColor3f(1,1,1);
-        if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);
     glPopMatrix();
+    glColor3f(0,1,0);
+    glutSolidTeapot(0.02);
     glutSwapBuffers();
 
 }
 int oldX = 0; int oldY = 0;
 void motion(int x , int y){
     teapotx += (x - oldX)/150.0;
-    teapoty += (y - oldY)/150.0;
+    teapoty -= (y - oldY)/150.0;
     oldX = x;
     oldY = y;
+    angle = x;
     printf("glTranslatef(%f, %f, 0);\n", teapotx, teapoty);
     glutPostRedisplay();
 }
@@ -96,7 +109,7 @@ int main(int argc, char** argv){
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("week12");
+    glutCreateWindow("week14");
 
     glutDisplayFunc(display);
     glutMouseFunc(mouse);
