28d6d3926bcda688f15b1c35f5c56f3921df8455
DorisTmaple <toffmaple@gmail.com>
2023-05-17 11:22:57 +0800

    DTang---week14_切換關節

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -5,12 +5,17 @@ GLMmodel * head = NULL;
 GLMmodel * body = NULL;
 GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
-int show[4] = {0,1,0,0};
+int show[4] = {1,1,1,1};
+int ID = 0; /// 0->頭、1->身、2->上手臂、3->下手臂
 void keyboard(unsigned char key, int x, int y){
-    if(key=='0') show[0] = !show[0];
-    if(key=='1') show[1] = !show[1];
-    if(key=='2') show[2] = !show[2];
-    if(key=='3') show[3] = !show[3];
+    if(key=='0') ID = 0;
+    if(key=='1') ID = 1;
+    if(key=='2') ID = 2;
+    if(key=='3') ID = 3;
+//    if(key=='0') show[0] = !show[0];
+//    if(key=='1') show[1] = !show[1];
+//    if(key=='2') show[2] = !show[2];
+//    if(key=='3') show[3] = !show[3];
     glutPostRedisplay();
 }
 
@@ -31,12 +36,23 @@ void display(){
             lowarmR = glmReadOBJ("Model/armhandR.obj");
             ///glmUnitize(body);
         }
+        if(ID == 0) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[0]) glmDraw(head, GLM_MATERIAL);
+
+        if(ID == 1) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[1]) glmDraw(body, GLM_MATERIAL);
         glPushMatrix();
             glTranslatef(teapotx, teapoty, 0);
+
+            if(ID == 2) glColor3f(1,0,0);
+            else glColor3f(1,1,1);
             if(show[2]) glmDraw(uparmR, GLM_MATERIAL);
         glPopMatrix();
+
+        if(ID == 3) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
