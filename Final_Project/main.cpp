9109d92a5b030fffade6c64cc2edc7f121616c31
DorisTmaple <toffmaple@gmail.com>
2023-05-24 12:13:46 +0800

    DTang---week15_攝影機

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -7,11 +7,30 @@ GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
 int show[4] = {0,1,1,1};
 int ID = 3; /// 0->頭、1->身、2->上手臂、3->下手臂
+
+FILE * fout = NULL;///week15-2 儲存關節位置
+FILE * fin = NULL;///week15-2 讀取關節位置
+float teapotx = 0, teapoty = 0;
+float angle[20] = {}; /// week15 修改為只旋轉特定關節/部位
+
+
 void keyboard(unsigned char key, int x, int y){
     if(key=='0') ID = 0;
     if(key=='1') ID = 1;
     if(key=='2') ID = 2;
     if(key=='3') ID = 3;
+    if(key=='s'){
+        if(fout == NULL) fout = fopen("motion.txt", "w"); ///week15-2 儲存關節位置
+        for(int i=0; i<20 ;i++){
+            fprintf(fout, "%.2f ", angle[i]);
+        }
+        fprintf(fout, "\n");
+    }else if(key=='r'){
+        if(fin == NULL) fin = fopen("motion.txt", "r");  ///week15-2 讀取關節位置
+        for(int i=0; i<20; i++){
+            fscanf(fin, "%f", &angle[i]);
+        }
+    }
 //    if(key=='0') show[0] = !show[0];
 //    if(key=='1') show[1] = !show[1];
 //    if(key=='2') show[2] = !show[2];
@@ -19,9 +38,6 @@ void keyboard(unsigned char key, int x, int y){
     glutPostRedisplay();
 }
 
-float teapotx = 0, teapoty = 0, angle = 0;
-FILE * fout = NULL;
-FILE * fin = NULL;
 
 void display(){
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
@@ -46,7 +62,7 @@ void display(){
 
         glPushMatrix();
             glTranslatef(-1.166666, 0.466667, 0);
-            glRotatef(angle,0,0,1);
+            glRotatef(angle[2] ,0 ,0 ,1);/// week15 修改為只旋轉特定關節/部位
             glTranslatef(1.166666, -0.466667, 0);
 
             if(ID == 2) glColor3f(1,0,0);
@@ -55,7 +71,7 @@ void display(){
 
             glPushMatrix();
                 glTranslatef(-1.933332, +0.133333, 0);
-                glRotatef(angle,0,0,1);
+                glRotatef(angle[3], 0, 0, 1);/// week15 修改為只旋轉特定關節/部位
                 glTranslatef(1.933332, -0.133333, 0);
 
                 if(ID == 3) glColor3f(1,0,0);
@@ -74,9 +90,10 @@ int oldX = 0; int oldY = 0;
 void motion(int x , int y){
     teapotx += (x - oldX)/150.0;
     teapoty -= (y - oldY)/150.0;
+    angle[ID] += (x - oldX);/// week15 修改為只旋轉特定關節/部位
     oldX = x;
     oldY = y;
-    angle = x;
+    ///angle = x;
     printf("glTranslatef(%f, %f, 0);\n", teapotx, teapoty);
     glutPostRedisplay();
 }
@@ -86,7 +103,7 @@ void mouse(int button, int state, int x, int y){
     if(state==GLUT_DOWN){
         oldX = x;
         oldY = y;
-        angle = x;
+        ///angle = x;
         ///teapotx = (x-150)/150.0;
         ///teapoty = (150-y)/150.0;
         //if(fout == NULL) fout = fopen("file4.txt", "w");
